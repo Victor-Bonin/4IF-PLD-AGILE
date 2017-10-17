5c7378845cd5fe68c5643c4979a3b279d0a6a160
Romain <rom.gouttefangeas@gmail.com>
2017-10-17 16:31:52 +0200

    Romain - Javadoc packages xml

--- PLD-Agile/src/xml/ExceptionXML.java
+++ PLD-Agile/src/xml/ExceptionXML.java
@@ -1,7 +1,15 @@
 package xml;
 
+/**
+ * Classe gerant les exceptions lors de l'ouverture et la deserialisation de xml
+ * @author 4104
+ */
 public class ExceptionXML extends Exception {
 
+	/**
+	 * Exception personnalisee pour la deserialisation de xml
+	 * @param message
+	 */
 	public ExceptionXML(String message) {
 		super(message);
 	}

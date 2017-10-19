bcd88451b0b52bc2985f557db1835e081b0ba2a7
unknown <pchauvet@IF501-204-06.insa-lyon.fr>
2017-10-19 17:19:54 +0200

    Pierrick : Modification de la sortie de calculTournee

--- PLD-Agile/src/modele/algo/TSP.java
+++ PLD-Agile/src/modele/algo/TSP.java
@@ -14,7 +14,7 @@ public interface TSP {
 	 * @param cout : cout[i][j] = duree pour aller de i a j, avec 0 <= i < nbSommets et 0 <= j < nbSommets
 	 * @param duree : duree[i] = duree pour visiter le sommet i, avec 0 <= i < nbSommets
 	 */
-	public void chercheSolution(int tpsLimite, int nbSommets, float[][] cout, int[] duree);
+	public Integer[] chercheSolution(int tpsLimite, int nbSommets, float[][] cout, int[] duree);
 	
 	/**
 	 * @param i

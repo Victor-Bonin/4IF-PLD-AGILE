dbadc099b33c74ea8237f6063043fff7655ba3f9
unknown <pchauvet@IF501-204-06.insa-lyon.fr>
2017-10-24 10:38:05 +0200

    Pierrick : Utilisation de map d'adjacence pour Dijkstra et clarification du code avec des hashmap

--- PLD-Agile/src/modele/algo/JSPtest.java
+++ PLD-Agile/src/modele/algo/JSPtest.java
@@ -3,7 +3,7 @@ package modele.algo;
 public class JSPtest {
 
 	public static void main(String[] args){
-		test4();
+
 	}
 	
 	public static void test1(){
@@ -15,7 +15,7 @@ public class JSPtest {
 						{1,4,5,0,2},
 						{3,3,6,1,0}
 						};
-		System.out.println("test, de 0 à 1 : "+cout[0][1]);
+		System.out.println("test, de 0 ï¿½ 1 : "+cout[0][1]);
 		int[] duree = {2,1,2,3,1};
 		int tpsLimite = 10000;
 		int nbSommets = 5;
@@ -52,63 +52,63 @@ public class JSPtest {
 		}
 	}
 	
-	public static void test3(){
-		Dijkstra dijkstra = new Dijkstra();
-		Float[][] graph = {{null,(float) 3,null,(float) 1,null,(float) 9,null},
-							{null,null,(float) 2,null,(float) 8,null,null},
-							{null,null,null,null,null,null,null},
-							{null,null,null,null,null,null,(float) 3},
-							{null,(float) 4,null,null,null,null,(float) 5},
-							{null,null,null,null,(float) 1,null,null},
-							{null,null,null,null,null,null,null}
-							};
-		int source = 0;
-		DjkSolution result = dijkstra.PCC(graph, source);
-		System.out.println("   dist  prev");
-		for(int i =0; i<7;i++){
-			System.out.println(i+" : "+result.dist[i]+"     "+result.prev[i]);
-		}
-	}
-	
-	public static void test4(){
-		TSP1 tsp = new TSP1();
-		Dijkstra dijkstra = new Dijkstra();
-		Float[][] graph = {{null,(float) 3,null,(float) 1,null,(float) 9,null},
-							{null,null,(float) 2,null,(float) 8,null,null},
-							{null,null,null,null,(float) 3,null,null},
-							{null,null,(float) 2,null,null,null,(float) 3},
-							{null,(float) 4,null,null,null,null,(float) 5},
-							{null,null,null,null,(float) 1,null,null},
-							{(float) 1,null,null,(float) 3,null,null,null}
-							};
-		int[] duree = {2,1,2,3,1,4,3};
-		float[][] cout = new float[7][7];
-		
-		for(int i=0; i<7; i++){
-			DjkSolution result = dijkstra.PCC(graph, i);	
-			
-			///////////////////////////
-			System.out.println("--"+i+"--");
-			System.out.println("   dist  prev");
-			for(int k =0; k<7;k++){
-				System.out.println(k+" : "+result.dist[k]+"     "+result.prev[k]);
-			}
-			System.out.println();
-			///////////////////////////
-			
-			for(int j=0; j<7; j++){
-				cout[i][j]=result.dist[j];
-			}
-		}
-		
-		int tpsLimite = 10000;
-		tsp.chercheSolution(tpsLimite, 7, cout, duree);
-		
-		System.out.println("temps de la solution : "+tsp.getCoutMeilleureSolution());
-		System.out.print("Chemin de la solution : ");
-		for(int i=0;i<7;i++){
-			System.out.print(tsp.getMeilleureSolution(i)+" ");
-		}
-	}
+//	public static void test3(){
+//		Dijkstra dijkstra = new Dijkstra();
+//		Float[][] graph = {{null,(float) 3,null,(float) 1,null,(float) 9,null},
+//							{null,null,(float) 2,null,(float) 8,null,null},
+//							{null,null,null,null,null,null,null},
+//							{null,null,null,null,null,null,(float) 3},
+//							{null,(float) 4,null,null,null,null,(float) 5},
+//							{null,null,null,null,(float) 1,null,null},
+//							{null,null,null,null,null,null,null}
+//							};
+//		int source = 0;
+//		DjkSolution result = dijkstra.PCC(graph, source);
+//		System.out.println("   dist  prev");
+//		for(int i =0; i<7;i++){
+//			System.out.println(i+" : "+result.dist[i]+"     "+result.prev[i]);
+//		}
+//	}
+//	
+//	public static void test4(){
+//		TSP1 tsp = new TSP1();
+//		Dijkstra dijkstra = new Dijkstra();
+//		Float[][] graph = {{null,(float) 3,null,(float) 1,null,(float) 9,null},
+//							{null,null,(float) 2,null,(float) 8,null,null},
+//							{null,null,null,null,(float) 3,null,null},
+//							{null,null,(float) 2,null,null,null,(float) 3},
+//							{null,(float) 4,null,null,null,null,(float) 5},
+//							{null,null,null,null,(float) 1,null,null},
+//							{(float) 1,null,null,(float) 3,null,null,null}
+//							};
+//		int[] duree = {2,1,2,3,1,4,3};
+//		float[][] cout = new float[7][7];
+//		
+//		for(int i=0; i<7; i++){
+//			DjkSolution result = dijkstra.PCC(graph, i);	
+//			
+//			///////////////////////////
+//			System.out.println("--"+i+"--");
+//			System.out.println("   dist  prev");
+//			for(int k =0; k<7;k++){
+//				System.out.println(k+" : "+result.dist[k]+"     "+result.prev[k]);
+//			}
+//			System.out.println();
+//			///////////////////////////
+//			
+//			for(int j=0; j<7; j++){
+//				cout[i][j]=result.dist[j];
+//			}
+//		}
+//		
+//		int tpsLimite = 10000;
+//		tsp.chercheSolution(tpsLimite, 7, cout, duree);
+//		
+//		System.out.println("temps de la solution : "+tsp.getCoutMeilleureSolution());
+//		System.out.print("Chemin de la solution : ");
+//		for(int i=0;i<7;i++){
+//			System.out.print(tsp.getMeilleureSolution(i)+" ");
+//		}
+//	}
 	
 }

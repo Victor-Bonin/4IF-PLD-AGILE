f972b963d4cdcf9a48caa3d6a1c026c2bc3c184a
Victor Bonin <victorduvercors@gmail.com>
2017-11-07 20:57:35 +0100

    Victor : Creation d'un html

--- PLD-Agile/src/modele/Tournee.java
+++ PLD-Agile/src/modele/Tournee.java
@@ -18,15 +18,16 @@ public class Tournee extends DemandeLivraison{
 		super.setEntrepot(entrepot);
 		super.setLivraisons(meilleureSolution);
 	}
-	
+
 	public Itineraire getItineraire(){
 		return itineraire;
 	}
-	
+
 	public boolean equals(Object obj) {
 		return this.getItineraire().equals(((Tournee)obj).getItineraire());
 	}
 
+	// TODO : recupérer les rues qui composent l'intersection + gérer rues inconnues + mise en page html + plages horaires + probleme évident avec les heures
 	public void exportFeuilleDeRoute() throws IOException {
 		this.getEntrepot().getHeureDepart();
 		String nomRue = "";
@@ -39,15 +40,13 @@ public class Tournee extends DemandeLivraison{
 		// Liste des chemins empruntés
 		List<Chemin> chemins = this.getItineraire().getChemins();
 		for (int i = 0; i < chemins.size(); i++) {
-			System.out.println(i);
-
 			// Liste des troncons empruntés sur ce chemin
 			List<Troncon> troncons = chemins.get(i).getTroncons();
 			for (int j = 0; j < troncons.size(); j = j + 1 + nbTronconsConseq) {
 
 				nomRue = troncons.get(j).GetNomRue();
 				longueurTroncon = troncons.get(j).getLongueur()/10;
-				long fin = troncons.get(j).getFin().getId(); // TODO recupérer les rues qui composent l'intersection
+				long fin = troncons.get(j).getFin().getId();
 				nbTronconsConseq = 0;
 				if (j < troncons.size()-1) {
 					while (nomRue.equals( troncons.get(j + nbTronconsConseq + 1).GetNomRue())){
@@ -60,26 +59,22 @@ public class Tournee extends DemandeLivraison{
 				}
 
 				longueurRue = String.valueOf(Math.round(longueurTroncon));
-				feuilleDeRoute += "Prenez la rue "+ nomRue +" pendant "+ longueurRue +"m \r\n";
+				feuilleDeRoute += "<br>Prenez la rue "+ nomRue +" pendant "+ longueurRue +"m \r\n </br>";
 			}
 			if (i < livraisons.size()){
-				feuilleDeRoute += "Livraison n° "+ (i+1) +" : Arrivée à "+ livraisons.get(i).getHeurePassage().HOUR_OF_DAY +"h"+ livraisons.get(i).getHeurePassage().MINUTE +", Durée : "+ livraisons.get(i).getDuree() +" \r\n";
+				if (livraisons.get(i) instanceof LivraisonPlageHoraire) {
+					feuilleDeRoute += "<br> <b> Livraison n° "+ (i+1) +" : Arriv&eacutee &agrave "+ livraisons.get(i).getHeurePassage().getTime().getHours() +"h "+ livraisons.get(i).getHeurePassage().getTime().getMinutes() +", Dur&eacutee : "+ (livraisons.get(i).getDuree()/60) +" min \r\n </b></br>";
+				}
+				else {
+				feuilleDeRoute += "<br> <b> Livraison n° "+ (i+1) +" : Arriv&eacutee &agrave "+ livraisons.get(i).getHeurePassage().getTime().getHours() +"h "+ livraisons.get(i).getHeurePassage().getTime().getMinutes() +", Dur&eacutee : "+ (livraisons.get(i).getDuree()/60) +" min \r\n </b></br>";
+				}
 			}
 		}
 
 		String pathname = "./Feuille de Route.html";
 		File file = new File(pathname);
-		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-
-		String contenu = "'<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'"
-				+ "'http://www.w3.org/TR/html4/loose.dtd'>"
-				+ "<html>"
-				+ "<head>"
-				+ "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8>"
-				+ "<title>$title</title>"
-				+ "</head>"
-				+ "<body>" + feuilleDeRoute + "</body>"
-				+ "</html>";
+		BufferedWriter bw = new BufferedWriter(new FileWriter(file));		
+		String contenu = "<html><head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'><title>Feuille de Route</title></head><body><p>"+ feuilleDeRoute +"</p></body></html>";
 		bw.write(contenu);
 		bw.close();
 

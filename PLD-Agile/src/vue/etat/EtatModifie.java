f9e347727155d2a87fcb7e538fa41d320dcc1e59
Romain <rom.gouttefangeas@gmail.com>
2017-11-09 13:55:18 +0100

    [Romain] Bouton supprimer accessible seulement apres calcul de la tournee

--- PLD-Agile/src/vue/etat/EtatModifie.java
+++ PLD-Agile/src/vue/etat/EtatModifie.java
@@ -5,20 +5,12 @@ import vue.Fenetre;
 import vue.VuePlan;
 import vue.VueTournee;
 
-public class EtatModifie extends EtatCalcule implements Etat {
+public class EtatModifie extends EtatCalcule {
 
 	@Override
 	public void afficherVue(Fenetre fenetre) {
+		super.afficherVue(fenetre);
 		VuePlan vuePlan = fenetre.getVuePlan();
-		VueTournee vueTournee = fenetre.getVueTournee();
-		Plan plan = fenetre.getPlan();
-		
-		if(plan.getDemandeLivraison().getLivraisons().isEmpty()) {
-			vuePlan.nettoyerIcones();
-		}else {
-			vueTournee.initTournee(plan.getDemandeLivraison());
-			vuePlan.afficherIcones(plan.getDemandeLivraison());
-		}
 		vuePlan.activerAnnulationBouton(true);
 	}
 }
